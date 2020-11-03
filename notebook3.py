175fb9062954ac037038958f11307ac51b9814df
teodorkasap <erol.erten@gmail.com>
2020-11-03 14:51:52 +0300

    first commit

---
+++ notebook3.py
@@ -0,0 +1,16 @@
+# %% - source of this notebook
+# https://medium.com/@aniruddha.choudhury94/part-2-bert-fine-tuning-tutorial-with-pytorch-for-text-classification-on-the-corpus-of-linguistic-18057ce330e1
+# in contrast to notebook1 and 2, this one does not freeze Bert parameters.
+# %% - imports etc.
+import torch
+# If there's a GPU available...
+if torch.cuda.is_available():
+    # Tell PyTorch to use the GPU.
+    device = torch.device("cuda")
+    print('There are %d GPU(s) available.' % torch.cuda.device_count())
+    print('We will use the GPU:', torch.cuda.get_device_name(0))
+# If not...
+else:
+    print('No GPU available, using the CPU instead.')
+    device = torch.device("cpu")
+
